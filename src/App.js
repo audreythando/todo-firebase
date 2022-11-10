09c38071a432c8e9940a9ed7d1e9a3767980c9df
audreythando <audreythando@gmail.com>
2022-11-10 10:34:22 +0200

    refactor: imported icon from react-icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import {AiOutlinePlus} from 'react-icons/ai'
 
 
 const style={
-  bg:`h-screen w-screen p-4 bg-gradient-to-r from `
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] `
 }
 
 function App() {
+const [todos , setTodos]= useState()
+
   return (
     <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+        <input className={style.input} type="text" placeholder="Add Todo" />
+<button className={style.button}><AiOutlinePlus size={30}/></button>
+</form>
+<ul></ul>
+      </div>
       
     </div>
   );

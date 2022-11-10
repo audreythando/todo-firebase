dc8b91e9fccda41d6685df98c41e1a86f809e209
audreythando <audreythando@gmail.com>
2022-11-10 11:38:15 +0200

    refactor: added styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import {AiOutlinePlus} from 'react-icons/ai'
-
+import Todo from "./Todo";
 
 const style={
-  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] `
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] `,
+  container:`bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 }
 
 function App() {
-const [todos , setTodos]= useState()
+const [todos , setTodos]= useState(['Learn React', 'Grind Leetcode' ])
 
   return (
     <div className={style.bg}>
@@ -17,7 +23,16 @@ const [todos , setTodos]= useState()
         <input className={style.input} type="text" placeholder="Add Todo" />
 <button className={style.button}><AiOutlinePlus size={30}/></button>
 </form>
-<ul></ul>
+<ul>
+{todos.map((todo, index) => (
+            <Todo
+              key={index}
+              todo={todo}
+            />
+          ))}
+
+</ul>
+<p className={style.count}>You have 2 todos</p>
       </div>
       
     </div>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {FaRegTrashAlt} from 'react-icons/fa'
 
-function Todo() {
+const style={
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+    row: `flex`,
+    text: `ml-2 cursor-pointer`,
+    textComplete: `ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`,
+
+}
+
+function Todo({todo}) {
   return (
-    <li>Todo</li>
+    <li className={style.li}>
+        <div className={style.row}>
+<input type="checkbox" />
+<p className={style.text}>{todo}</p>
+</div>
+<button>{<FaRegTrashAlt/>}</button>
+    </li>
   )
 }
 
